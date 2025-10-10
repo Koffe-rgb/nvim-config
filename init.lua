6f8c2447444d435c9a62eb65cbdd11bac22ac008
koffe <niccolum129@gmail.com>
2025-10-11 02:47:06 +0500

    commented out my plugin in init.lua

--- init.lua
+++ init.lua
@@ -1,4 +1,4 @@
 require("config.lazy")
 
-local cs_namespace = require("myplugins.cs-namespace")
-cs_namespace.setup()
+-- local cs_namespace = require("myplugins.cs-namespace")
+-- cs_namespace.setup({})
